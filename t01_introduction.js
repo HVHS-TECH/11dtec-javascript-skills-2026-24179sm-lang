1f516efb214c7450a1bb34cf0960285a4409c516
24179sm-lang <24179sm@hvhs.school.nz>
2026-04-30 02:26:55 +0000

    Apr 30, 2026, 2:26 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -3,7 +3,7 @@ This is a block comment.
 Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(Godwork)
+console.log("Gods Work")
 
 // This is a single line comment
 
