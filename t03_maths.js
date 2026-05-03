9b9d86086034dab8dccb5c975062cca0b9315a41
24179sm-lang <24179sm@hvhs.school.nz>
2026-05-03 21:54:56 +0000

    May 3, 2026, 9:54 PM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,22 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task:Task 2
+****************************/
+console.log("Task 2")
+console.log("Semiti")
+console.log("15")
+console.log("pocketmoney")
+console.log("2026")
+
+
+
+
+/***************************
+Main code
+****************************/
+
+
+
+
+/***************************
+Functions
+****************************/
