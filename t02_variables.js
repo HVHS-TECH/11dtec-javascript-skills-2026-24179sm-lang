47393675b53e22b854bd13ec19fe148525eee23f
24179sm-lang <24179sm@hvhs.school.nz>
2026-05-03 21:28:44 +0000

    May 3, 2026, 9:28 PM

--- t02_variables.js
+++ t02_variables.js
@@ -2,10 +2,9 @@
 Name of task:Task 2
 ****************************/
 console.log("Task 2")
-
 console.log("Semiti")
 console.log("15y/o")
-console.log("How much pocket money the user has")
+console.log("pocketmoney")
 
 
 
