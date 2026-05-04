4bf9663d84145210086f114c782eea453117c98a
24179sm-lang <24179sm@hvhs.school.nz>
2026-05-04 22:19:12 +0000

    May 4, 2026, 10:19 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 3, Maths here */
+/**************************
+Name of Task 5
+/************************** */
+console.log("Task 3,")
+console.log("Semiti") 
+console.log( 15, )
+console.log( 125, )
+console.log( 2025, )
+console.log("Tongan")
+console.log( "As of you are 15, years old ")
+console.log( "You have 125, dollars" )
+console.log(" You were born in Tonga" )
+console.log("In 10 years you will be 25 years old ")
+console.log(" You spend half of your money, now you have 62 dollars ")
+console.log(" Then you get $3, now you have 65 dollars ")
+
+//variables
+
+/********************** */
+Main
+/**********************/
+
+
+
+
+/******************************
+ functions
+ ******************************/
