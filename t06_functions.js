0e019a27e41d30c89c621f8efc7f60f4c0a62f40
24179sm-lang <24179sm@hvhs.school.nz>
2026-05-10 21:13:17 +0000

    May 10, 2026, 9:13 PM

--- t06_functions.js
+++ t06_functions.js
@@ -23,7 +23,7 @@ console.log(" Then you get $3, now you have 66 dollars ")
 //variables
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 
-OUTPUT.innerHTML = "<h2>Hello world</h2>"
+OUTPUT.innerHTML = "<h2>Welcome</h2>"
 OUTPUT.innerHTML += "<p>Hello Semiti</p>";
 OUTPUT.innerHTML += "<p>Hello I'm Semiti and I love to play volleyball and rugby because im HIM </p>"
 OUTPUT.innerHTML += "<p>And being the man I am I enjoy playing with friends</p>"
