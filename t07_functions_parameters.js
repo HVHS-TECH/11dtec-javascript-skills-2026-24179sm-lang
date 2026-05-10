f7e4cdd743ad46c04078ac265d47809cd35cc641
24179sm-lang <24179sm@hvhs.school.nz>
2026-05-10 21:44:59 +0000

    May 10, 2026, 9:44 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,14 @@
-/* Paste your code from task 6 here */
+// Your Name
+// Functions with Parameters
+
+console.log("Displaying products");
+
+// Function with two parameters
+function displayProduct(_Semiti, _) {
+    console.log(_name + ": $" + _price);
+}
+
+// Display the products
+displayProduct("Chocolate bar", 4);
+displayProduct("Chips", 3);
+displayProduct("Drink", 2.50);
